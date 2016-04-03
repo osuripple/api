df793351c098d2605d61c0d6a785fa3cce70c8a3
Howl <git@howl.moe>
2016-04-03 20:31:51 +0200

    omitempty for message in response

--- common/response.go
+++ common/response.go
@@ -1,7 +1,8 @@
 package common
 
+// Response is the data that is always returned with an API request.
 type Response struct {
-	Code int `json:"code"`
-	Message string `json:"message"`
-	Data interface{} `json:"data"`
+	Code    int         `json:"code"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data"`
 }
