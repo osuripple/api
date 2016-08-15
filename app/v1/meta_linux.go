55d7621ef56e8a5b58b31bf14f624ea80d959cd3
Howl <the@howl.moe>
2016-08-15 21:56:29 +0200

    remove -o api, as it's the evil

--- app/v1/meta_linux.go
+++ app/v1/meta_linux.go
@@ -80,7 +80,7 @@ func MetaUpdateGET(md common.MethodData) common.CodeMessager {
 		if !execCommand("go", "get", "-v", "-u", "-d") {
 			return
 		}
-		if !execCommand("bash", "-c", "go build -v -ldflags \"-X main.Version=`git rev-parse HEAD`\" -o api") {
+		if !execCommand("bash", "-c", "go build -v -ldflags \"-X main.Version=`git rev-parse HEAD`\"") {
 			return
 		}
 
